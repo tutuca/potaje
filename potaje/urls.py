141da9092f69a2f212eb4845aa96b7e7ce8d334b
Matías Iturburu <maturburu@gmail.com>
2014-06-21 00:21:20 -0300

    SEO stuff

--- potaje/urls.py
+++ potaje/urls.py
@@ -1,7 +1,19 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+from django.http import HttpResponse
+from django.contrib.sitemaps import GenericSitemap
+from albums.models import Album
 admin.autodiscover()
+
+info_dict = {
+    'queryset': Album.objects.all(),
+    'date_field': 'created',
+}
+
+sitemaps = {
+    'leaks': GenericSitemap(info_dict, priority=0.6),
+}
+
 
 urlpatterns = patterns('albums.views',
     url(r'^$', 'home', name='home'),
@@ -10,5 +22,10 @@ urlpatterns = patterns('albums.views',
 )
 
 urlpatterns += patterns('',
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^lobby/', include(admin.site.urls)),
+    (r'^robots\.txt$', lambda r: HttpResponse(
+        "User-agent: *\nDisallow: /media/*\nDisallow: /lobby/*", mimetype="text/plain")),
+    (r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', 
+        {'sitemaps': sitemaps})
+
 )
