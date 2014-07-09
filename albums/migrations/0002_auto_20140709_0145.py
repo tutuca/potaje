dd4b8f9c66aa12d2982a8ed56b905e887b20bac6
Matías Iturburu <maturburu@gmail.com>
2014-07-08 22:50:15 -0300

    Agrego migraciones y datos iniciales

---
+++ albums/migrations/0002_auto_20140709_0145.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('albums', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Content',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('source', models.URLField(blank=True, null=True)),
+                ('image', models.ImageField(blank=True, upload_to='pictures', null=True)),
+                ('kind', models.SmallIntegerField(default=10, choices=[(10, 'Picture'), (20, 'Video')])),
+                ('caption', models.CharField(blank=True, max_length=128, null=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('thumbnail', models.URLField(editable=False)),
+                ('code', models.TextField(editable=False)),
+                ('album', models.ForeignKey(to='albums.Album')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.RemoveField(
+            model_name='picture',
+            name='album',
+        ),
+        migrations.DeleteModel(
+            name='Picture',
+        ),
+    ]
