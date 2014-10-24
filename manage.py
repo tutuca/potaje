9d22247cc68dc2f27bc7ff52c8ca6b89ba6cc9ac
Matías Iturburu <maturburu@gmail.com>
2014-10-24 15:46:57 -0300

    Nicer manage.py

--- manage.py
+++ manage.py
@@ -2,9 +2,12 @@
 import os
 import sys
 
-if __name__ == "__main__":
+
+def do_manage():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "potaje.settings")
-
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    do_manage()
