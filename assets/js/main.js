b8fd51c5d3f0f68e9e62b0da2db25c2e50723827
Matías Iturburu <maturburu@gmail.com>
2014-11-28 13:46:51 -0300

    Prueba de aplicación. Contiene:
    - Collections para `Section` y `Album`.
    - Vista de navegación.
    - Router.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,55 +1,27 @@
 var $ = require('jquery');
 $(function() {
     'use strict';
-
     var _ = require('underscore'),
         Backbone = require('backbone'),
-        AlbumTemplate = require('./templates/album.hbs');
-
-    var NavView = Backbone.View.extend({
-         el: $("nav ul"),
-         
-    });
-    var Album = Backbone.Model.extend({
-        url: '/api/albums/',
-    });
-    var Albums = Backbone.Collection.extend({
-        url: '/api/albums/',
-        model: Album,
-    });
-    var AlbumView = Backbone.View.extend({
-        el: $("nav ul"),
-        model: Album,
-        template: new AlbumTemplate(),
-        initialize: function () {
-            require('slick-carousel');
-            this.listenTo(this.model, "change", this.render);
-        },
-        render: function () {
-            var content = this.template(this.model.attributes);
-            this.$el.html(content).slick();
-
-            return this;
-        },
-        show: function (event) {
-        }
-    });
  
-    var Router = new Backbone.Router.extend({
+    var Router = Backbone.Router.extend({
         rutes: {
             'home': 'home',
-            'album/:album_id:': 'album'
+            'album/:album_id': 'album'
+        },
+        home: function ( ) {
         },
         album: function (album_id) {
-            return new AlbumView();
-        },
-        home: function(){
-        },
+
+        }
+
     });
- 
-    // Main entry point.    
-    return Backbone.history.start({
-        pushState: true,
-        hashChange: false
+    var app_router = new Router();
+    app_router.on('initialize:after', function() {
+        return Backbone.history.start({
+            pushState: true,
+            hashChange: false
+        });    
     });
+
 });
