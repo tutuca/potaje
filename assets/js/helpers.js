6eb4e67c735aa19cda077ca91c5bd5f498f87446
Matías Iturburu <maturburu@gmail.com>
2015-02-18 19:58:21 -0300

    Arreglé el "algoritmo" para hacer encajar las imágenes al contenedor

---
+++ assets/js/helpers.js
@@ -0,0 +1,78 @@
+/* Helpers */
+function hexToRgb(hex) {
+    /*
+    * Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    * from http://stackoverflow.com/a/5624139/53468
+    */
+    'use strict';
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
+function toRGBAString (color, a) {
+    'use strict';
+    if (!a) {a=1;}
+    return 'rgba('+color.r+','+color.g+','+color.b+','+a+')';
+}
+
+function viewport(){
+    /*
+    * Calculates the available surface on the client's window.
+    * */
+    'use strict';
+    var e = window,
+        a = 'inner';
+    if ( !( 'innerWidth' in window ) ){
+        a = 'client';
+        e = document.documentElement || document.body;
+    }
+    var innerWidth = Math.max(e[a+'Width'], 580),
+        innerHeight = Math.max(e[a+'Height'], 580);
+    return { width : innerWidth , height : innerHeight };
+}
+
+function fit(selector, to, padding){
+    'use strict'
+    var padding = padding || 0,
+        visible_height, visible_width,
+        old_height, old_width,
+        delta, landscape;
+    if (to) {
+        visible_height = $(to).height() - padding;
+        visible_width = $(to).width() - padding;
+    } else {
+        var vp = viewport();
+        visible_height = vp.height - padding;
+        visible_width = vp.width- padding;
+
+    }
+    $(selector).each(function() {
+        var $el = $(this);
+        old_width = $el.width();
+        old_height = $el.height();
+        landscape = old_width > old_height;
+        if (landscape) {
+            $el.css({
+                width: Math.floor(visible_height / old_height * old_width),
+                height: 'auto'
+            });
+        } else {
+            $el.css({
+                height: visible_height,
+                width: 'auto'
+            });
+        }
+        /* delta = (visible_width - $el.width())/2;
+        $(this).css('margin-left', delta); */
+    })
+
+};
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,65 +1,22 @@
+/* Globals */
 var selected,
+    slide,
+    styles = [
+        '#67e2ad',
+        '#003e5f',
+        '#fa8e53',
+        '#f84c53'
+    ];  // TODO: generate this using http://devmag.org.za/2012/07/29/how-to-choose-colours-procedurally-algorithms/
     slickOptions = {
-    dots: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    centerMode: true,
-    variableWidth: false
+        dots: true,
+        accessibility: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true
     };
-function hexToRgb(hex) {
-    /*
-    * Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-    * from http://stackoverflow.com/a/5624139/53468
-    */
-    'use strict';
-    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-        return r + r + g + g + b + b;
-    });
 
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-    } : null;
-}
-function toRGBAString (color, a) {
-    'use strict';
-    if (!a) {a=1;}
-    return 'rgba('+color.r+','+color.g+','+color.b+','+a+')';
-}
-function viewport(){
-    /*
-    * Calculates the available surface on the client's window.
-    * */
-    'use strict';
-    var e = window,
-        a = 'inner';
-    if ( !( 'innerWidth' in window ) ){
-        a = 'client';
-        e = document.documentElement || document.body;
-    }
-    var innerWidth = Math.max(e[a+'Width'], 580),
-        innerHeight = Math.max(e[a+'Height'], 580);
-    return { width : innerWidth , height : innerHeight };
-}
-function renderSection(){
-    /*
-    * Pjax callback funcion
-    * */
-    'use strict';
-    if (selected){
-        $('body').css({'overflow':'hidden'});
-        $('nav').toggle('fade', 1000);
-
-        $('.work a').not(selected).each(function(index){
-            $(this).parent('article').toggle('fade', 1500*index);
-        });
-        return $('#slides').slick(slickOptions);
-    }
-}
 function getSlidesContainer(caller){
     "use strict";
     var container = $('#slide-wrapper');
@@ -70,51 +27,96 @@ function getSlidesContainer(caller){
     return container;
 }
 
+
+/* Views */
+var section = {
+    // TODO: move it out.
+    // TODO: This is not a proper "class". Should need modifications to be actually useful.
+    init: function(event){
+        event.preventDefault();
+        event.stopPropagation();
+        selected = this; // TODO: move to local scope.
+
+        var hash = window.location.hash,
+            target = $(this).attr('href'),
+            container = getSlidesContainer();
+        section.fetch(target, container, '#slide-wrapper')
+        $('.control').one('click', section.close);
+    },
+    fetch: function(target, container, fragment) {
+        "use strict";
+        $.pjax({
+            url:this.target,
+            container: this.container,
+            fragment: this.fragment,
+            success: function() {
+                slide = section.render(); // TODO: move to local scope.
+            }
+        });
+
+    },
+    render: function(){
+        /*
+        * Pjax callback funcion
+        * */
+        'use strict';
+        if (selected){
+            $('body').css({'overflow':'hidden'});
+            $('nav').toggle('fade', 1000);
+
+            $('.work a').not(selected).each(function(index){
+                $(this).parent('article').toggle('fade', 1500*index);
+            });
+            return $('#slides').slick(slickOptions);
+        }
+    },
+
+    close: function(event) {
+        event.stopPropagation();
+        $('nav').toggle('fade');
+        slide.slickRemove(); // TODO: move `slide` to local scope.
+        history.pushState({}, "Potaje ", "/"+hash);
+
+    }
+}
+function collapseNavbar (){
+    "use strict";
+    debugger
+    $('#main-nav').hide('fold');
+}
+
+/* main */
 $(function(){
     'use strict';
-    var vp = viewport(),
-        styles = ['#67e2ad','#003e5f','#fa8e53','#f84c53'],  // generate this
-        slide = $('#slides').slick(slickOptions);
+    var vp = viewport();
 
-    $('section').css({'min-height': vp.height});
+    $('.albums').css({'min-height': vp.height});
+    $('.slider').css({'height': vp.height});
+    $('.slide').css({
+        'height': vp.height - 120,
+        'width': vp.width - 120
+    });
+
     $("header").affix();
 
     $('body').scrollspy({ target: '#main-nav' });
     $('nav a').each(function(i){
-        var target = $(this).attr('href'),
+        var target = $(this).attr('href').replace('/', ''),
             style = styles[(i%styles.length)];
         $(target).css({'background-color':toRGBAString(hexToRgb(style), 0.5)});
         $(this).css({'background-color':style});
     });
+    $('.slide img').on('load', function(){
+        fit(this, '#slide-wrapper');
+    });
+
+    slide = $('#slides')
+                .slick(slickOptions)
+                .on('init', function(){console.log('caca')});
+
     setTimeout(function(){
         var new_color = styles[Math.floor(Math.random()*styles.length)];
         $('#logo').css('background-color', new_color);
     }, 5000);
-    /*
-    $('.cover').on("click", function(event){
-        event.preventDefault();
-        event.stopPropagation();
-
-        var selected = this,
-            hash = window.location.hash,
-            target = $(this).attr('href'),
-            container = getSlidesContainer();
-        $.pjax({
-            url:target,
-            container: container,
-            fragment: '#slide-wrapper',
-            success: function() {
-
-                slide = renderSection(selected);
-            }
-        });
-        $('.control').one('click',function(e) {
-            e.stopPropagation();
-            $('nav').toggle('fade');
-            slide.slickRemove();
-            history.pushState({}, "Potaje ", "/"+hash);
-
-        });
-    });
-    */
+    //$('.cover').on("click", section.init);
 });
