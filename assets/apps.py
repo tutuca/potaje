e66dd99adfab646c1d17e10796a41d6c84c3cd32
Matías Iturburu <maturburu@gmail.com>
2015-09-02 01:09:28 -0300

    Hago de assets una app django y la registro en settings.py a fines de disponer de sus archivos estáticos desde django.

---
+++ assets/apps.py
@@ -0,0 +1,7 @@
+# rock_n_roll/apps.py
+
+from django.apps import AppConfig
+
+class AssetsConfig(AppConfig):
+    name = 'assets'
+    verbose_name = "Choclos pal potaje"
--- potaje/settings.py
+++ potaje/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'albums',
     'profiles',
-
+    'assets'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -98,10 +98,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
