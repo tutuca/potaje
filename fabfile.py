68fa25b15c86bf8a1426feaee04e61aa3e80a95b
tutuca <maturburu@gmail.com>
2017-12-11 21:08:17 -0300

    Ignoring build files

--- fabfile.py
+++ fabfile.py
@@ -25,6 +25,21 @@ env.bundle = "%s-static.tar.gz" % (
     env.project_name,
 )
 
+def staging(username, host):
+    """Staging environment."""
+    env.hosts = host
+    env.user = username
+    env.base_dir = BASEDIR
+    env.static_root = os.path.join(env.base_dir, 'static_%s' % env.project_name)
+    env.deploy_dir = os.path.join(env.base_dir, 'build', env.project_name)
+    env.virtual_env = os.path.join(env.base_dir, 'venvs', env.project_name
+    env.server_command = os.path.join(
+        env.base_dir,
+        'webapps',
+        env.project_name,
+        'apache2/bin/restart'
+    )
+
 def production(username, host, base_dir):
     """Production environment."""
     env.hosts = host
@@ -42,9 +57,9 @@ def production(username, host, base_dir):
 
 def release(rev='HEAD'):
     """Create a tarball, uploads it and decompresses it in the rigth path."""
-    require("host", provided_by=[production])
-    require("deploy_dir", provided_by=[production])
-    require("virtual_env", provided_by=[production])
+    require("host", provided_by=[production, staging])
+    require("deploy_dir", provided_by=[production, staging])
+    require("virtual_env", provided_by=[production, staging])
     tar = env.tar
     local("git archive %s -o %s" % (rev, tar))
     put(tar, tar)
@@ -60,7 +75,7 @@ def build_static_bundle():
     # XXX: This expects the virtualenv to be activated already.
     # XXX: overcome that limitation using `prefix()` and context managers.
 
-    require("project_name", provided_by=[production])
+    require("project_name", provided_by=[production, staging])
     require("static_dir")
     local("npm start")
     local("manage collectstatic --noinput")
@@ -75,8 +90,8 @@ def upload_static(force=False):
     This will attempt to create and delete local gzips.
     Cleanup is not always what it should...
     """
-    require("project_name", provided_by=[production])
-    require("static_root", provided_by=[production])
+    require("project_name", provided_by=[production, staging])
+    require("static_root", provided_by=[production, staging])
     if not exists(env.bundle) or force:
         build_static_bundle()
     put(env.bundle, env.bundle)
@@ -85,5 +100,5 @@ def upload_static(force=False):
 
 def apache_restart():
     """Restart the program in the servers."""
-    require("server_command", provided_by=[production])
+    require("server_command", provided_by=[production, staging])
     run(env.server_command)
